d77a8e1858fbb1c451373a86d0ad42e22aef1db3
Dom Barnes <dom@dombarnes.com>
2016-06-05 17:48:07 +0100

    Fixing Redis config stuff

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,4 +1,4 @@
-if Rails.env == 'production'
+unless Rails.env == 'development'
   uri = URI.parse(ENV["REDIS_URL"])
   Resque.redis = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
 end
