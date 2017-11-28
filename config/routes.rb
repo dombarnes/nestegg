566d821d604942f4f9d332232508a9af98191331
Dom Barnes <dom@dombarnes.com>
2017-11-28 22:13:37 +0000

    Updating nokogiri

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 Rails.application.routes.draw do
   resources :accounts do  
   	resources :transactions
+    resources :categories
     resources :import, only: [:new] do 
       collection {post :create }
     end
   end
-  resources :transactions, only: :index do 
+  resources :transactions, only: [:index, :show, :edit] do 
     collection do 
       get :search
     end
